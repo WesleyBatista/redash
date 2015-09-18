8cb8fab20b8a3789e86e9588824a46b0616b739b
wesleybatista <wesley.batista@easytaxi.com.br>
2015-09-18 18:39:59 -0300

    Add comments to help configuring

--- setup/cron_postgres_backup.sh
+++ setup/cron_postgres_backup.sh
@@ -1,3 +1,10 @@
 FILE_NAME="postgres_backup_"`date +"%Y%m%d_%H%M%S"`".sql.gzip"
 sudo -u postgres pg_dumpall | gzip > ${FILE_NAME}
 gsutil cp ${FILE_NAME} gs://redash/postgres/${FILE_NAME}
+
+
+# set with `crontab -e`
+# ```bash
+# * * * * * /opt/redash/current/setup/cron_postgres_backup.sh 1> /opt/redash/logs/cron_job.log 2> /opt/redash/logs/cron_job_errors.log
+# @hourly /opt/redash/current/setup/cron_postgres_backup.sh 1> /opt/redash/logs/cron_job.log 2> /opt/redash/logs/cron_job_errors.log
+# ```
